63f94a09e2e3d4304dfd3eb503c7bdc12e6204d0
Yusuf Morsy <yumo2500@colorado.edu>
2025-04-25 19:00:00 -0600

    questionnaire stuff added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TailorIQLandingPage from './landingPage';
+import QuestionnaireSection from './components/questionnaire';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TailorIQLandingPage />
+    
   </React.StrictMode>
 );
